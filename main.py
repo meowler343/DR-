aed3f83c66d2d556f9e832982347efa76b9b8120
meowler343 <marckbasmanov@ya.ru>
2026-03-19 21:43:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+def dr_compiler(source_code):
+    lines = source_code.split('\n')
+    for line in lines:
+        if line.startswith("fn"):
+            print(f"[DR#] Найдена функция: {line[3:]}")
+        if "let" in line:
+            print(f"[DR#] Объявление переменной: {line.strip()}")
+        if "asm" in line:
+            print("[DR#] ВНИМАНИЕ: Вход в режим ASM (NASM)")
 
-print("Hello DR#")
+# Тест нового синтаксиса
+code = """
+fn main
+    let x = 10
+    asm
+"""
+dr_compiler(code)
